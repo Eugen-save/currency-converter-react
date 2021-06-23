c08ea985b8c70ada138b0705fc93ee8942a18de3
Evgeny Savelyev <savelyev.eugeny@gmail.com>
2021-06-23 06:44:04 +0200

    currencies js added

---
+++ src/currencies.js
@@ -0,0 +1,20 @@
+const currencies =
+    [
+        {
+            id: "EUR",
+            rate: 4.5366,
+            name: "Euro (EUR)",
+        },
+        {
+            id: "USD",
+            rate: 3.8232,
+            name: "Dolar amerykański (USD)",
+        },
+        {
+            id: "CZK",
+            rate: 0.1776,
+            name: "Korona czeska (CZK)"
+        },
+    ];
+
+export default currencies;
