c7f5455c6a4ae8c39e09c7f3528471f428e9fe82
Evgeny Savelyev <savelyev.eugeny@gmail.com>
2021-06-23 06:42:47 +0200

    Footer added

---
+++ src/Footer/index.js
@@ -0,0 +1,9 @@
+import "./style.css";
+
+const Footer = () => (
+    <footer className="footer">
+        Kursy wyliczane są na podstawie Tabela nr 118/A/NBP/2021 z dnia 2021-06-22
+    </footer>
+);
+
+export default Footer;
