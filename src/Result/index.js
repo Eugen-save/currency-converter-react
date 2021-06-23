e64fbd3b28deae8bdc2cba2b0562768aea187990
Evgeny Savelyev <savelyev.eugeny@gmail.com>
2021-06-23 06:43:22 +0200

    result interaction added

---
+++ src/Result/index.js
@@ -0,0 +1,15 @@
+import "./style.css"
+
+const Result = ({setAmount, result}) => (
+   <p className="result">
+       <span></span>
+       {result !== undefined && (
+           <>
+           {result.sourceAmount.toFixed(2)} PLN =
+           <strong>{result.finalResult.toFixed(2)} {result.currency}</strong>
+           </>
+       )}
+   </p>
+);
+
+export default Result;
