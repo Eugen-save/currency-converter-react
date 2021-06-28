7278e48f7af7e99015b10a8dd0c026fadf430be9
Evgeny Savelyev <savelyev.eugeny@gmail.com>
2021-06-28 05:38:13 +0200

    index css moved to styled js as Global Style

--- src/styled.js
+++ src/styled.js
@@ -2,9 +2,25 @@ import { createGlobalStyle } from "styled-components";
 import background from "./background.png"
 
 export const GlobalStyle = createGlobalStyle`
+
+html {
+  box-sizing: border-box;
+}
+
+*, ::after, ::before {
+  box-sizing: inherit;
+}
+
 body{
-    background-image: url("${background}");
-    background-attachment: fixed;
+     font-family: 'Lato', sans-serif;
+     font-weight: 400;
+     font-size: 24px;
+     color: #0f4c81;
+     max-width: 900px;
+     margin: 0 auto;
+     padding: 0 20px;
+     background-image: url("${background}");
+     background-attachment: fixed;
 
     @media (max-width: 767px) {
         background-position: center;
