9a1d5c6ed6159b2d1880790489b43b0002ccbe71
Evgeny Savelyev <savelyev.eugeny@gmail.com>
2021-07-05 15:04:35 +0200

    Created animation style for Loading message

--- src/Form/index.js
+++ src/Form/index.js
@@ -4,6 +4,7 @@ import Button from "./Button";
 import Result from "../Result";
 import { useState } from "react";
 import { useGetCurrencies } from "./useGetCurrencies";
+import { TextLoading, LoadingContainer, Circle, Animation } from "./Message/Loading/styled";
 
 const Form = () => {
 
@@ -32,11 +33,31 @@ const Form = () => {
             setAmount("");
         }
     };
-
-
     switch (status) {
         case "loading":
-            return <p>Strona się laduje</p>;
+            return (
+                <>
+                    <LoadingContainer>
+                        <Circle>
+                            <Animation circle1></Animation>
+                            <Animation circle2></Animation>
+                            <Animation circle3></Animation>
+                            <Animation circle4></Animation>
+                            <Animation circle5></Animation>
+                            <Animation circle6></Animation>
+                            <Animation circle7></Animation>
+                            <Animation circle8></Animation>
+                            <Animation circle9></Animation>
+                            <Animation circle10></Animation>
+                            <Animation circle11></Animation>
+                            <Animation circle12></Animation>
+                        </Circle>
+                        <TextLoading>
+                            Strona jest w trakcie pobierania danych z Europiejskiego Banku Centralnego. Prośba o chwilę cierpliwości :)
+                        </TextLoading>
+                    </LoadingContainer>
+                </>
+            );
         case "error":
             return <p>Coś poszło nie tak</p>
         case "success":
