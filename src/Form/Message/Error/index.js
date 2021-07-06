fc86623fe4c10ac6b810d632049261d177fdc8ff
Evgeny Savelyev <savelyev.eugeny@gmail.com>
2021-07-06 06:34:43 +0200

    added message Error

---
+++ src/Form/Message/Error/index.js
@@ -0,0 +1,10 @@
+import { Text } from "../styled"
+
+const Error = () => (
+    <Text>Prawdopodobnie nie masz połączenia internetowego.
+        Jeżeli jednak masz połączenie, a strona nie działa,
+        to oznacza że trwają prace serwsowe i błąd wkrótce będzie naprawiony
+    </Text>
+);
+
+export default Error;
